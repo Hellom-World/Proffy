b73da7561f5ce768c5c497ce036ae1c8176e20dd
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-08-04 04:05:25 +0100

    Dia 1 Concluido

---
+++ src/pages/TeacherList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles.css'
+import PageHeader from '../../componentes/PageHeader'
+
+function TeacherList(){
+    return(
+        <div id="page-teacher-list" className="container">
+          <PageHeader title="Estes são os priffys disponíveis."/>
+        </div>
+    )
+}
+
+export default TeacherList;
